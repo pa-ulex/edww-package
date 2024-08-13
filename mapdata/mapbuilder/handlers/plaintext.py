b68068a7024e12e92bf931ea86bd1360c12333ec
Manuel Pick <manuel.pick98@gmail.com>
2024-08-13 17:47:35 +0200

    Delete Local mapbuilder installation

--- mapdata/mapbuilder/handlers/plaintext.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-
-class PlainTextHandler:
-    def handle(self, item: Path) -> str:
-        with item.open(encoding="iso-8859-1") as input:
-            return input.read()
